3f6ab4a0ce6ca38029a4eb118a4dc727667b073c
AlanLee0928 <91860974+AlanLee0928@users.noreply.github.com>
2021-11-08 18:43:06 +0800

    Update Class01.java

--- Class01.java
+++ Class01.java
@@ -2,6 +2,7 @@ package classtest;
 
 class Cal_area{
 	int a , b , c , d;
+	double e = 4;
 	
 	void show(){
 		System.out.println(this.Volume());
@@ -9,14 +10,14 @@ class Cal_area{
 	}
 	
 	double Volume(){
-		return (4 / 3) * 3.14159 * a * a * a;
+		return Math.round(e / 3 * 3.14159 * a * a * a * 10.0) / 10.0;
 	}
 	int area() {
 		return b * c * 2 + c * d * 2 + d * b * 2;
 	}
 }
 
-public class Class01 {
+public class test {
 	public static void main(String[] args) {
 		Cal_area ar = new Cal_area();
 		ar.a = 10;
