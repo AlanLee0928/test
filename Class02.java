8d54b5414ff458037959b2b8680c924896243df4
AlanLee0928 <91860974+AlanLee0928@users.noreply.github.com>
2021-11-08 18:50:36 +0800

    Update Class02.java

--- Class02.java
+++ Class02.java
@@ -6,20 +6,25 @@ class is_prime{
 	int a , b;
 	
 	void prime() {
-		for(int i = 2 ; i <= (a / 2) ; i++) {
-			if(a % i == 0) {
-				b = 0;
-				break;
-			}
-			else {
-				b = 1;
-			}
-		}
-		if(b == 1) {
+		if(a == 2 || a == 3) {
 			System.out.println("YES");
 		}
 		else {
-			System.out.println("No");
+			for(int i = 2 ; i <= (a / 2) ; i++) {
+				if(a % i == 0) {
+					b = 0;
+					break;
+				}
+				else {
+					b = 1;
+				}
+			}
+			if(b == 1) {
+				System.out.println("YES");
+			}
+			else {
+				System.out.println("No");
+			}
 		}
 	}
 }
@@ -27,8 +32,10 @@ class is_prime{
 public class Class02 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		is_prime pr = new is_prime();
-		pr.a = sc.nextInt();
-		pr.prime();
+		while(sc.hasNext()){
+			is_prime pr = new is_prime();
+			pr.a = sc.nextInt();
+			pr.prime();
+		}
 	}
 }
